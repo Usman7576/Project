3172988409fed2be04a2e930d9f21423a7c6eb02
Mujtaba <137613459+emmj0@users.noreply.github.com>
2024-12-04 02:41:58 +0500

    Delete test_model_training.py

--- test_model_training.py
+++
@@ -1,8 +0,0 @@
-# tests/test_model_training.py
-import pytest
-from src.model import IrisModel
-
-def test_model_training():
-    model = IrisModel('datasets/iris.csv')
-    accuracy = model.train()
-    assert accuracy is not None  # Ensure that the model returns an accuracy score

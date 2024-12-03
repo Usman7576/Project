edba6c9be7770707c4590f17456b34b4771376b2
Mujtaba <137613459+emmj0@users.noreply.github.com>
2024-12-04 02:41:01 +0500

    Delete test_data_loading.py

--- test_data_loading.py
+++
@@ -1,9 +0,0 @@
-# tests/test_data_loading.py
-import pytest
-import pandas as pd
-from src.model import IrisModel
-
-def test_data_loading():
-    model = IrisModel('datasets/iris.csv')
-    assert isinstance(model.data, pd.DataFrame)  # Ensure that data is loaded as a DataFrame
-    assert not model.data.empty  # Ensure that the DataFrame is not empty
